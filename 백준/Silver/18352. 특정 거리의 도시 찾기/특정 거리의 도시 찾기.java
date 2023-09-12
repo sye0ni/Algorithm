ba57e3c7b876a34f3ce898c9550f68f6c89f245e
seungyeon lim <91115868+sye0ni@users.noreply.github.com>
2023-09-13 00:26:37 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1248 ms, Memory: 259148 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -6,17 +6,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+/**
+ * bfs
+ * 327812kb 1476ms
+ */
 public class Main {
 
     static ArrayList<Integer>[] graph;
     static int[] visited;
+    static int K;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
-        int K=Integer.parseInt(st.nextToken());
+        K=Integer.parseInt(st.nextToken());
         int X=Integer.parseInt(st.nextToken());
         StringBuilder sb=new StringBuilder();
         visited=new int[N];
@@ -32,10 +37,9 @@ public class Main {
 
         BFS(X-1);
         for(int i=0;i<N;i++) {
-//            System.out.println(visited[i]);
             if(visited[i]==K) sb.append((i+1)+"\n");
         }
-        if(sb.length()==0) System.out.println(-1);
+        if(sb.length()==0) System.out.println(-1); // 찾는 도시가 없다면 -1 출력
         else System.out.println(sb);
     }
 
@@ -51,8 +55,9 @@ public class Main {
                 int c=graph[pop].get(i);
                 if(visited[c]==-1){
                     visited[c]=visited[pop]+1;
+                    if(visited[c]==K) continue; // 여기서 갈 수 있는 점들은 어차피 거리가 K보다 커지기 때문에 큐에 담아서 더 탐색할 필요가 없다 
                     queue.addLast(c);
-                }// 방문하지 않은 점이면
+                }// 방문하지 않은 점이면 큐에 추가
             }
 
         }
