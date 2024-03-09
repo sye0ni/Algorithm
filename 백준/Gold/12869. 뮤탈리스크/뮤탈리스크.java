de2da226de4e2e84bc38bca72fff2b4edef40f76
seungyeon lim <91115868+sye0ni@users.noreply.github.com>
2024-03-09 16:28:38 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 96 ms, Memory: 13772 KB -BaekjoonHub

---
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ *  N 의 개수가 1-3 개 이므로, 완탐으로 구한다. => 시간초과
+ *  이미 계산한 값에 대해서는 다시 계산할 필요가 없기 때문에 
+ */
+
+
+public class Main {
+    static int[][][] dp;
+    static int[][] attacks = {{9, 3, 1}, {9, 1, 3}, {3, 9, 1}, {3, 1, 9}, {1, 3, 9}, {1, 9, 3}};
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        int[] scv = new int[3];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            scv[i] = Integer.parseInt(input[i]);
+        }
+
+        dp = new int[61][61][61]; // dp[a][b][c]: 첫 번째 SCV가 a, 두 번째 SCV가 b, 세 번째 SCV가 c일 때의 최소 공격 횟수
+        for (int i = 0; i <= 60; i++) {
+            for (int j = 0; j <= 60; j++) {
+                for (int k = 0; k <= 60; k++) {
+                    dp[i][j][k] = -1; // 초깃값
+                }
+            }
+        }
+
+        int result = solve(scv[0], scv[1], scv[2]);
+        System.out.println(result);
+    }
+
+    static int solve(int a, int b, int c) {
+        if (a <= 0 && b <= 0 && c <= 0) return 0;
+
+        if (dp[a][b][c] != -1) return dp[a][b][c];
+
+        dp[a][b][c] = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 6; i++) {
+            int newA = Math.max(0, a - attacks[i][0]);
+            int newB = Math.max(0, b - attacks[i][1]);
+            int newC = Math.max(0, c - attacks[i][2]);
+            dp[a][b][c] = Math.min(dp[a][b][c], solve(newA, newB, newC) + 1);
+        }
+
+        return dp[a][b][c];
+    }
+}
