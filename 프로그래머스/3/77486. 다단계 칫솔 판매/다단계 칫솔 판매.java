b1d6b229a25f0c76e6654bb74b7afa2173dcf7cf
임승연 <dlatmddus980820@gmail.com>
2024-06-17 18:41:34 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 54.83 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -4,8 +4,8 @@ import java.util.*;
 // 각 판매원을 다단계 조직에 참여시킨 다른 판매원의 이름을 담은 배열 referral
 
 // 아래 -> 위를 가리키는 방향으로 그래프가 만들어져야함 
-// hashmap<String, List<String>> 으로 저장 ? 
-// referral 없으면 center 가리키게 
+// hashmap<String, List<String>> 으로 저장 
+// referral 없으면 center 
 
 class Solution {
     
@@ -43,23 +43,18 @@ class Solution {
             count=amount[i];
             
             calculate(name,count);
-            // for(int j=0;j<answer.length;j++) {
-            //     System.out.print(answer[j]+" ");
-            // }
-            // System.out.println();
         }
         
         
         return answer;
     }
     
-    // name 이 count 만큼 팔았다면 ? 
+    // name 이 count 만큼 팔았다
     void calculate(String name, int count) {
         int idx=index.get(name); 
         int income=count*100;
         
         while(true) {
-            // System.out.println("income: "+income);
             idx=index.get(name);
             
             if(income*0.1<1) {  // 절사 불가능
