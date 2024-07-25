b321ebdb828c4a9dcb6dc9f96d88adbbcc59d328
임승연 <dlatmddus980820@gmail.com>
2024-07-25 16:31:15 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,19 +1,23 @@
+// 그리디 
+// 끝자리가 5면 앞자리를 봐서 올림 혹은 버림 
+// 끝자리가 5보다 크면 올림, 5보다 작으면 버림 
+
 class Solution {
     public int solution(int storey) {
-        int answer = 0;
+        int answer=0;
         
-        while (storey > 0) {
-            int digit = storey % 10;
-            storey /= 10;
+        while (storey>0) { // 0으로 만들기 
+            int digit = storey%10; // 끝자리 숫자  
+            storey/=10; 
             
-            if (digit > 5) {
-                answer += (10 - digit);
-                storey++; // 올림 처리
-            } else if (digit == 5 && (storey % 10) >= 5) {
-                answer += (10 - digit);
-                storey++; // 올림 처리
-            } else {
-                answer += digit;
+            if (digit>5) { // 올림 
+                answer+=(10-digit);
+                storey++; 
+            } else if (digit==5 && (storey%10>=5)) { // 올림 
+                answer+=(10 - digit);
+                storey++; 
+            } else { // 버림 
+                answer+=digit;
             }
         }
         
