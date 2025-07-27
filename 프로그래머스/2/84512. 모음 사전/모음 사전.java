15f82a127e0017623b8858ef3fc85a0668ca8f66
임승연 <dlatmddus980820@gmail.com>
2025-07-27 21:19:46 +0900

    [level 2] Title: 모음 사전, Time: 30.28 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,47 +1,22 @@
 import java.util.*;
 
-// 중복순열 이용 
 class Solution {
-    
-    static int count=0;
-    static boolean flag=false;
-    
+    static List<String> list = new ArrayList<>();
+    static char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+
     public int solution(String word) {
-     //   int answer = 0;
-        
-        String input="";
-        for(int i=0;i<word.length();i++){
-            if(word.charAt(i)=='A') input+="0";
-            else if(word.charAt(i)=='E') input+="1";
-            else if(word.charAt(i)=='I') input+="2";
-            else if(word.charAt(i)=='O') input+="3";
-            else input+="4";
-        }
-        System.out.println(input);
-        search("",input); // input이 될 때까지 
-        return count;
+        dfs("", 0);
+        return list.indexOf(word) + 1;
     }
-    
-    static void search(String curr,String word){
-     //  System.out.println(curr);
-        if(flag) return;
-        
-        if(curr.equals(word)){ // 조건 만족 
-            flag=true;
-            return;
+
+    // 모든 가능한 조합 생성
+    private void dfs(String current, int depth) {
+        if (depth > 5) return;
+
+        if (!current.isEmpty()) list.add(current);
+
+        for (int i = 0; i < 5; i++) {
+            dfs(current + vowels[i], depth + 1);
         }
-        
-        count++;
-        
-        if(curr.length()==5){ // 가지치기 
-            return; 
-        }
-        
-        for(int i=0;i<5;i++){
-            curr+=Integer.toString(i);
-            search(curr,word);
-            curr=curr.substring(0,curr.length()-1);
-        }
-        
     }
 }
