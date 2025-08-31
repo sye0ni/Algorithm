d5ec02eb94dfd00cbc064acc8b7c6df92a041b5f
임승연 <dlatmddus980820@gmail.com>
2025-08-31 22:55:41 +0900

    [level 2] Title: 무인도 여행, Time: 7.71 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,88 +1,84 @@
 import java.util.*;
 
-/*
-* 숫자가 있는 점에서 bfs 하여 값을 구하고, X 로 초기화 하는 과정 반복 
-* 
-* 많이 풀어봤던 그래프 탐색 문제인데, 프로그래머스에서 풀어보려고 골랐다! 
-* 생각보다 자동완성 없이 푸는데 익숙하지 않음
-*
-***** 
-* 어제는 stringbuilder 를 까먹고,, 오늘은 문자열 길이를 구할때 length가 아닌 length() 라는거,, 
-*/
 
 class Solution {
-
-    List<Integer> days=new ArrayList<>(); 
-    char[][] graph;
-    int[] dx=new int[]{-1,1,0,0};
-    int[] dy=new int[]{0,0,-1,1};
-    int garo,sero;
+    
+    static PriorityQueue<Integer> pq=new PriorityQueue<>();  // 각 섬 생존 날짜 저장
+    static Character[][] graph;
+    static boolean[][] isVisited;
+    static Deque<int[]> queue=new ArrayDeque<>(); 
+    static int[] dx=new int[]{-1,1,0,0};
+    static int[] dy=new int[]{0,0,-1,1};
+    static int sero,garo;
     
     public int[] solution(String[] maps) {
-        int[] answer; // 리턴은 배열로 
-        
-        graph=new char[maps.length][maps[0].length()];
-        garo=maps[0].length();
         sero=maps.length;
+        garo=maps[0].length();
         
-        for(int i=0;i<maps.length;i++){
-            for(int j=0;j<maps[i].length();j++){
+        graph=new Character[sero][garo];
+        isVisited=new boolean[sero][garo];
+        
+        for(int i=0;i<sero;i++) {
+            for(int j=0;j<garo;j++) {
                 graph[i][j]=maps[i].charAt(j);
             }
-        } // 그래프 생성 
+        } // 그래프  생성
         
-        for(int i=0;i<maps.length;i++){
-            for(int j=0;j<maps[i].length();j++){
-                if(graph[i][j]!='X'){
+        for(int i=0;i<sero;i++) {
+            for(int j=0;j<garo;j++) {
+                if(graph[i][j]!='X' && !isVisited[i][j]) {
                     bfs(i,j);
                 }
             }
-        } // 그래프 생성 
-        
-        if(days.size()==0) {
-            answer=new int[1];
-            answer[0]=-1;
         }
-        else{
-            answer=new int[days.size()];
-            Collections.sort(days);
-            for(int i=0;i<days.size();i++){
-                answer[i]=days.get(i);
+        
+        int[] answer;
+        // pq 에 있는 것들 빼서 배열 만들기 
+        if(pq.size()==0) {
+            answer=new int[]{-1};
+        }
+        else {
+            answer=new int[pq.size()];
+            for(int i=0;i<answer.length;i++) {
+                answer[i]=pq.poll();
             }
         }
         
         return answer;
     }
     
-    public void bfs(int x,int y){
-        int sum=0;
-        int currX,currY;
-    
+    static void bfs(int x,int y) { // 시작점 지정 
         
-        Deque<int[]> queue=new ArrayDeque<>();
-        queue.add(new int[]{x,y});
-        sum+=graph[x][y]-'0'; 
-        graph[x][y]='X';
+        queue.clear(); 
+        queue.addLast(new int[]{x,y}); 
+        isVisited[x][y]=true; 
         
-        while(!queue.isEmpty()){
-            int[] pop=queue.removeFirst();
-            x=pop[0];
-            y=pop[1];
+        int currX,currY; 
+        int nextX,nextY;
+        int[] temp;
+        int cnt= graph[x][y]-'0'; // 문자를 숫자로 
+        
+        while(!queue.isEmpty()) {
+            temp=queue.removeFirst();
+            currX=temp[0];
+            currY=temp[1];
             
-            for(int i=0;i<4;i++){
-                currX=x+dx[i];
-                currY=y+dy[i];
-                if(currX<0 || currX>=sero || currY<0 || currY>=garo) continue;
-                if(graph[currX][currY]=='X') continue; 
-                queue.add(new int[]{currX,currY});
-                sum+=graph[currX][currY]-'0'; 
-                graph[currX][currY]='X';
+            for(int i=0;i<4;i++) {
+                nextX=currX+dx[i];
+                nextY=currY+dy[i];
+                
+                if(nextX<0 || nextX>=sero || nextY<0 || nextY>=garo || isVisited[nextX][nextY] || graph[nextX][nextY]=='X') {
+                    continue;
+                }
+                
+                isVisited[nextX][nextY]=true;
+                queue.addLast(new int[]{nextX,nextY});
+                cnt+=graph[nextX][nextY]-'0';
             }
             
         }
         
-        days.add(sum);
+        pq.add(cnt);
     }
-    
     
 }
